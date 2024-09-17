90e770e8a32376dfaf018345b00931aa9bdc9cd9
dzawanalifi <dzakwan624@gmail.com>
2024-09-18 01:10:28 +0700

    update

--- amilapp.py
+++ amilapp.py
@@ -17,7 +17,7 @@ import branca
 
 st.set_page_config(layout="wide")
 
-locale.setlocale(locale.LC_ALL, 'id_ID')
+locale.setlocale(locale.LC_ALL, 'id_ID.UTF-8')
 
 st.header("Beranda Si Amil Zakatkuy", divider="green")
 
